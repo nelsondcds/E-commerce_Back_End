b143b8e1e441b3eed756d259f99a786967db768a
nelsondcds <pipoca012008@yahoo.com>
2021-09-05 01:28:17 -0400

    server.js updated to sync Sequelize models on server start, models set up, associations set up

--- models/Tag.js
+++ models/Tag.js
@@ -5,16 +5,24 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		tag_name: {
+			type: DataTypes.STRING,
+			allowNull: false // isn't listed as a requirement but makes sense that it can't be null
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'tag',
+	}
 );
 
 module.exports = Tag;
