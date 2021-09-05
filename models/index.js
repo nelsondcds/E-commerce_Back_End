e400d985d71ccb5d556f37818edba59a9f492480
nelsondcds <pipoca012008@yahoo.com>
2021-09-05 03:46:10 -0400

    routes completed and associations updated

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  	foreignKey: 'category_id'
+  	foreignKey: 'category_id',
+	onDelete: 'cascade'
 });
 
 // Categories have many Products
@@ -17,12 +18,22 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
 	through: ProductTag,
+	foreignKey: 'product_id',
+	onDelete: 'cascade'
+});
+
+Product.hasMany(ProductTag, {
 	foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
 	through: ProductTag,
+	foreignKey: 'tag_id',
+	onDelete: 'cascade'
+});
+
+Tag.hasMany(ProductTag, {
 	foreignKey: 'tag_id'
 });
 
